8c8de2ecf2ac76f46c9c88015a0ae23a60417915
Imer Perdana <mrprdnid@gmail.com>
2019-11-29 14:52:55 +0700

    nambahin form session

--- openacademy/models.py
+++ openacademy/models.py
@@ -18,6 +18,9 @@ class Course(models.Model):
     		('done', 'Done'),
     	], default='draft')
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -27,3 +30,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
